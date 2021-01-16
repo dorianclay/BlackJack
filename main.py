1081c353b19f9cebe4879eb112dab1fa20089858
Dorian Clay <dorian@dorianclay.com>
2021-01-16 11:25:36 -0800

    Implement game deck to draw cards.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from deck import Deck
+from game_deck import GameDeck
 
 def main():
   print('Welcome to Dorian\'s Black Jack.')
-  main_deck = Deck()
-  for card in main_deck.cards:
-    print(card)
+  main_deck = GameDeck([Deck(), Deck()])
+  for iter in range(10):
+    print(main_deck.draw())
 
 if __name__ == '__main__':
   main()
