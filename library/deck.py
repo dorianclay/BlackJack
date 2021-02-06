000fe01c69cc68aecdf2da7a27868c5cfc8b012b
Ben Rooke <benrooke@verily.com>
2021-02-06 09:41:04 -0800

    Test deck class.

--- library/deck.py
+++ library/deck.py
@@ -1,11 +1,11 @@
 from library.card import Card
 from library.card import Suit
 
+CARDS_PER_SUIT = 13
+
 class Deck(object):
   def __init__(self):
     self.cards = []
     for suit in Suit:
-      for card_value in range(1, 14):
-        new_card = Card(card_value, suit)
-        self.cards.append(new_card)
-        
+      suited_cards = [Card(value, suit) for value in range(1, CARDS_PER_SUIT + 1)]
+      self.cards.extend(suited_cards)
