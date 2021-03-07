cb393699edaf82b8cea467d8c7deb569666a132c
Ben Rooke <benrooke@verily.com>
2021-03-07 12:16:49 -0800

    Create Menu to handle choice actions.

---
+++ library/cli/menu.py
@@ -0,0 +1,12 @@
+class Menu:
+  def __init__(self, choices):
+    self.choices = choices
+
+  def make_choice(self, index):
+    self.choices[index].action()
+
+  def __str__(self):
+    menu_list = []
+    for index, choice in enumerate(self.choices):
+      menu_list.append(f'{index + 1}: {choice}')
+    return '\n'.join(menu_list)
