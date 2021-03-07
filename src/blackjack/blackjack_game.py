2c1f26b983d65ef19f959a3121a203498633081b
Ben Rooke <benrooke@verily.com>
2021-03-06 18:15:18 -0800

    Remove prompt logic from game model.

--- src/blackjack/blackjack_game.py
+++ src/blackjack/blackjack_game.py
@@ -10,31 +10,19 @@ class BlackjackGame(Game):
   def __init__(self, dealer, players):
     super().__init__(dealer, players)
     self.current_player_index = 0
-    self.action_map = {
-      'Hit': self.hit_player,
-      'Stay': self.next_player,
-    }
-
-  # Player should engage BlackJackGame, rather than Game asking player to take a turn.
-  def take_turn(self):
-    if not self.player_turns_left():
-      return
-    player = self.players[self.current_player_index]
-    # Use dependency injection so BlackjackGame doesn't know about CLI Prompt.
-    # e.g. so that if we have a web version, the request can come from a different source than CLI.
-    prompt = Prompt(f'{player.name}\'s turn.', self.action_map)
-    prompt.act()
 
   def hit_player(self):
-    player = self.players[self.current_player_index]
+    player = self.current_player()
     self.dealer.draw(player)
     player_points = evaluate_points(player.cards)
     if player_points > MAX_POINTS:
-      print('You bust!')
-      self.next_player()
-  
-  def next_player(self):
+      self.end_current_players_turn()
+
+  def current_player(self):
+    return self.players[self.current_player_index]
+
+  def end_current_players_turn(self):
     self.current_player_index = self.current_player_index + 1
 
-  def player_turns_left(self):
-    return self.current_player_index < len(self.players)
+  def is_game_over(self):
+    return self.current_player_index >= len(self.players)
