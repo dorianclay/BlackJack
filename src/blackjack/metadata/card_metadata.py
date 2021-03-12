7ecaf58c79246b4d839d50a3d043038c76fe3f7f
Ben Rooke <benrooke@verily.com>
2021-03-12 11:01:51 -0800

    Create card metadata class.

--- src/blackjack/metadata/card_metadata.py
+++ src/blackjack/metadata/card_metadata.py
@@ -1,18 +1,29 @@
 class CardMetadata:
-  def __init__(self, card):
-    self.card = card
+
+  class Keys:
+    value = 'value'
+    suit = 'suit'
+    is_hidden = 'is_hidden'
+
+  def __init__(self, value, suit):
+    self.value = value
+    self.suit = suit
     self.is_hidden = False
 
   def json_repr(self):
     if self.is_hidden:
       return self.json_repr_hidden()
     return {
-      'value': self.card.valToFace(),
-      'suit': self.card.suitToString(),
-      'is_hidden': False,
+      CardMetadata.Keys.value: self.value,
+      CardMetadata.Keys.suit: self.suit,
+      CardMetadata.Keys.is_hidden: False,
     }
 
   def json_repr_hidden(self):
     return {
-      'is_hidden': True,
+      CardMetadata.Keys.is_hidden: True,
     }
+
+  @staticmethod
+  def from_card(card):
+    return CardMetadata(card.valToFace(), card.suitToString())
