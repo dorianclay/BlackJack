e581c0f04a75b702e0081078cd10967e1add6cb5
Ben Rooke <benrooke@verily.com>
2021-03-12 11:03:27 -0800

    Test utilities to aid in testing.

---
+++ test/testutils/card_metadata_utils.py
@@ -0,0 +1,8 @@
+from src.blackjack.metadata.card_metadata import CardMetadata
+
+def suits_from_card_json(cards_json):
+  return sorted([card_json[CardMetadata.Keys.suit] for card_json in cards_json])
+
+
+def values_from_card_json(cards_json):
+  return sorted([card_json[CardMetadata.Keys.value] for card_json in cards_json])
