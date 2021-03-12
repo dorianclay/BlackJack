02740550944cb4ad1b944d801bb9ea245d18b276
Ben Rooke <benrooke@verily.com>
2021-03-12 11:02:17 -0800

    Create player metadata class.

--- src/blackjack/metadata/player_metadata.py
+++ src/blackjack/metadata/player_metadata.py
@@ -2,30 +2,42 @@ from src.blackjack.point_evaluator import evaluate_points
 from src.blackjack.metadata.card_metadata import CardMetadata
 
 class PlayerMetadata:
-  def __init__(self, name, card_metadata):
+
+  class Keys:
+    name = 'name'
+    score = 'score'
+    their_turn = 'their_turn'
+    cards = 'cards'
+
+  def __init__(self, name, score, card_metadata):
     self.name = name
     # Score is hidden by default.
-    self.score = None
+    self.show_score = False
+    self.score = score
     self.card_metadata = card_metadata
     self.is_their_turn = False
 
   def json_repr(self):
-    return {
-      'name': self.name,
-      'score': self.score,
-      'their_turn': self.is_their_turn,
-      'cards': [metadata.json_repr() for metadata in self.card_metadata]
+    json = {
+      PlayerMetadata.Keys.name: self.name,
+      PlayerMetadata.Keys.their_turn: self.is_their_turn,
+      PlayerMetadata.Keys.cards: [metadata.json_repr() for metadata in self.card_metadata]
     }
+    if self.show_score:
+      json[PlayerMetadata.Keys.score] = self.score
+    return json
 
   def reveal_all_cards(self):
     # Reveal the score as well.
-    self.score = evaluate_points(player.cards)
+    self.show_score = True
     for metadata in self.card_metadata:
       metadata.is_hidden = False
 
   @staticmethod
   def from_player(player):
-    card_metadata = [CardMetadata(card) for card in player.cards]
+    score = evaluate_points(player.cards)
+    card_metadata = [CardMetadata.from_card(card) for card in player.cards]
     # Hide a player's first card.
-    card_metadata[0].is_hidden = True
+    if len(card_metadata) > 0:
+      card_metadata[0].is_hidden = True
     return PlayerMetadata(player.name, score, card_metadata)
