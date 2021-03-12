8b3952d0e2f4cb2d47119064b73e65c3bb523f76
Ben Rooke <benrooke@verily.com>
2021-03-12 11:05:11 -0800

    Add view classes to display cards and info in CLI

---
+++ src/blackjack/cli/player_view.py
@@ -0,0 +1,19 @@
+from src.blackjack.cli.card_view import draw_cards_json
+
+def draw_player_view(player_json):
+  is_current_player = player_json['their_turn']
+  draw_name(player_json, is_current_player)
+  draw_score(player_json, is_current_player)
+  draw_cards_json(player_json['cards'])
+
+def draw_name(player_json, is_current_player):
+  name_prefix = ''
+  if is_current_player:
+    name_prefix = 'You: '
+  name = player_json['name']
+  print(f'{name_prefix}{name}')
+
+def draw_score(player_json, is_current_player):
+  if is_current_player:
+    score = player_json['score']
+    print(f'Score: {score}')
