640113c409a03cb227922c7367aad3bb22da4f2f
Ben Rooke <benrooke@verily.com>
2021-03-12 11:02:34 -0800

    Create game metadata class.

--- src/blackjack/metadata/game_metadata.py
+++ src/blackjack/metadata/game_metadata.py
@@ -1,17 +1,26 @@
 from src.blackjack.metadata.player_metadata import PlayerMetadata
+from src.blackjack.metadata.results_metadata import ResultsMetadata
 
 class GameMetadata:
-  def __init__(self, player_metadata, other_players_metadata, is_game_over):
+
+  class Keys:
+    you = 'you'
+    players = 'players'
+    results = 'results'
+
+  def __init__(self, player_metadata, other_players_metadata, results_metadata):
     self.player_metadata = player_metadata
     self.other_players_metadata = other_players_metadata
-    self.is_game_over = is_game_over
+    self.results_metadata = results_metadata
 
   def json_repr(self):
-    return {
-      'is_game_over': self.is_game_over,
-      'you': self.player_metadata.json_repr(),
-      'players': [metadata.json_repr() for metadata in self.other_players_metadata]
+    json = {
+      GameMetadata.Keys.you: self.player_metadata.json_repr(),
+      GameMetadata.Keys.players: [metadata.json_repr() for metadata in self.other_players_metadata],
     }
+    if self.results_metadata is not None:
+      json[GameMetadata.Keys.results] = self.results_metadata.json_repr()
+    return json
 
   def reveal_all_cards(self):
     for metadata in self.other_players_metadata:
@@ -19,14 +28,24 @@ class GameMetadata:
 
   @staticmethod
   def from_game(game, main_player):
-    main_player_metadata = None
-    other_players_metadata = []
-    for player in game.players:
-      metadata = PlayerMetadata.from_player(player)
-      metadata.is_their_turn = player == game.current_player()
-      if player == main_player:
-        main_player_metadata = metadata
-        main_player_metadata.reveal_all_cards()
-        continue
-      other_players_metadata.append(metadata)
-    return GameMetadata(main_player_metadata, other_players_metadata, game.is_game_over())
+    other_players = GameMetadata.get_other_players(game.players, main_player)
+    other_players_metadata = [GameMetadata.create_player_metadata(game, player) for player in other_players]
+    main_player_metadata = GameMetadata.create_player_metadata(game, main_player)
+    main_player_metadata.reveal_all_cards()
+    return GameMetadata(
+        main_player_metadata,
+        other_players_metadata,
+        ResultsMetadata.from_game(game))
+
+  @staticmethod
+  def get_other_players(players, main_player):
+    return [player for player in players if player != main_player]
+
+  @staticmethod
+  def create_player_metadata(game, player, is_main_player=False):
+    metadata = PlayerMetadata.from_player(player)
+    if game.is_players_turn(player):
+      metadata.is_their_turn = True
+    if is_main_player:
+      metadata.reveal_all_cards()
+    return metadata
