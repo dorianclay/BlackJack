82c99cf8771878517195ae73c9d114deca86d2a3
Ben Rooke <benrooke@verily.com>
2021-03-13 04:34:08 -0800

    Set up continuous integration.

--- lint.sh
+++ lint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 sh scripts/install_yapf.sh
-sh scripts/run_yapf.sh
+sh scripts/run_yapf.sh "${1:--i}"
--- scripts/run_yapf.sh
+++ scripts/run_yapf.sh
@@ -3,4 +3,4 @@
 DIR=$(dirname "${BASH_SOURCE[0]}")
 source "${DIR}/paths.rc"
 
-find . -type f -name "*.py" ! -path "./${YAPF_TARGET}/*" | xargs yapf-files/bin/yapf --in-place --style="google"
+find . -type f -name "*.py" ! -path "./${YAPF_TARGET}/*" | xargs yapf-files/bin/yapf --style="google" "$1"
