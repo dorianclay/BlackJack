62a7a39d3810eea316bf29c8d1be2baa797990aa
Ben Rooke <benrooke@verily.com>
2021-03-13 04:56:22 -0800

    Simplify the linting effort.

--- scripts/install_yapf.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DIR=$(dirname "${BASH_SOURCE[0]}")
-source "${DIR}/paths.rc"
-
-if [ ! -e "$YAPF_TARGET" ]; then
-  pip3 install --install-option="--prefix=" --target "$YAPF_TARGET" yapf
-fi
