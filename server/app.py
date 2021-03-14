663f1c1256f8a2add02aecbe0a7df22072ebf0f6
Ben Rooke <benrooke@verily.com>
2021-03-13 21:43:41 -0800

    Full fledged server API tests.

--- server/app.py
+++ server/app.py
@@ -1,9 +1,10 @@
-import uuid
+from uuid import UUID
 
 from flask import Flask, request
 from library.deck import Deck
 from library.game_deck import GameDeck
 from library.player import Player
+from server.room import Room
 from src.blackjack.blackjack_dealer import BlackjackDealer
 from src.blackjack.blackjack_game import BlackjackGame
 from src.blackjack.metadata.game_metadata import GameMetadata
@@ -11,47 +12,57 @@ from src.blackjack.validation import Validation
 
 app = Flask(__name__)
 
-main_deck = GameDeck([Deck()])
-players = [Player('Computer')]
-dealer = BlackjackDealer(main_deck)
-game = BlackjackGame(dealer, players)
-validation = Validation(game)
+room = Room()
 
 
-def get_player(player_id):
+def get_player(player_id, game):
     for player in game.players:
         if player.id == player_id:
             return player
 
 
-@app.route('/blackjack/api/v1/players/<player_id>/games/<game_id>')
+@app.route('/blackjack/api/v1/games', methods=['POST'])
+def create_game():
+    main_deck = GameDeck([Deck()])
+    dealer = BlackjackDealer(main_deck)
+    game = BlackjackGame(dealer, [])
+    room.add_game(game)
+    return str(game.id)
+
+
+@app.route('/blackjack/api/v1/games/<game_id>/players/<player_id>')
 def get_game(game_id, player_id):
-    # TODO: Utilize game_id.
-    player = get_player(uuid.UUID(player_id))
+    game = room.get_game(UUID(game_id))
+    player = get_player(UUID(player_id), game)
     if player is None:
         return '', 502
     meta = GameMetadata.from_game(game, player)
     return meta.json_repr()
 
 
-@app.route('/blackjack/api/v1/players/<player_id>/hit')
-def hit_player(player_id):
-    if validation.player_can_hit(player_id):
+@app.route('/blackjack/api/v1/games/<game_id>/players/<player_id>/hit')
+def hit_player(game_id, player_id):
+    game = room.get_game(UUID(game_id))
+    validation = Validation(game)
+    if validation.player_can_hit(UUID(player_id)):
         game.hit_player()
         return '', 204
     return '', 502
 
 
-@app.route('/blackjack/api/v1/players/<player_id>/stay')
-def stay_player(player_id):
-    if validation.is_players_turn(player_id):
+@app.route('/blackjack/api/v1/games/<game_id>/players/<player_id>/stay')
+def stay_player(game_id, player_id):
+    game = room.get_game(UUID(game_id))
+    validation = Validation(game)
+    if validation.is_players_turn(UUID(player_id)):
         game.end_current_players_turn()
         return '', 204
     return '', 502
 
 
-@app.route('/blackjack/api/v1/players', methods=['POST'])
-def create_player():
+@app.route('/blackjack/api/v1/games/<game_id>/players', methods=['POST'])
+def create_player(game_id):
+    game = room.get_game(UUID(game_id))
     name = request.json['name']
     new_player = Player(name)
     game.players.append(new_player)
--- test/server/app_tests.py
+++ test/server/app_tests.py
@@ -1,11 +1,125 @@
 import unittest
 
 from server.app import *
+from src.blackjack.metadata.game_metadata import GameMetadata
+from src.blackjack.metadata.player_metadata import PlayerMetadata
+from uuid import UUID
 
 
 class AppTestCase(unittest.TestCase):
-    # TODO: Add tests. Need to provide API for creating game first.
-    pass
+
+    def test_creating_game(self):
+        with app.test_client() as c:
+            response = c.post('/blackjack/api/v1/games')
+            game_id = UUID(response.data.decode())
+            game = room.get_game(game_id)
+            self.assertEqual(game_id, game.id)
+
+    def test_adding_player_to_game(self):
+        with app.test_client() as c:
+            response = c.post('/blackjack/api/v1/games')
+            game_id = UUID(response.data.decode())
+            response = c.post(f'/blackjack/api/v1/games/{game_id}/players',
+                              json={'name': 'Ben Rooke'})
+            player_id = UUID(response.data.decode())
+            game = room.get_game(game_id)
+            player = game.players[0]
+            self.assertEquals(1, len(game.players))
+            self.assertEquals('Ben Rooke', player.name)
+            self.assertEquals(player_id, player.id)
+
+    def test_player_hit(self):
+        with app.test_client() as c:
+            response = c.post('/blackjack/api/v1/games')
+            game_id = UUID(response.data.decode())
+            response = c.post(f'/blackjack/api/v1/games/{game_id}/players',
+                              json={'name': 'Ben Rooke'})
+            player_id = UUID(response.data.decode())
+            game = room.get_game(game_id)
+            player = game.players[0]
+            self.assertEqual(0, len(player.cards))
+            response = c.get(
+                f'/blackjack/api/v1/games/{game_id}/players/{player_id}/hit')
+            self.assertEqual(1, len(player.cards))
+
+    def test_player_hit_but_not_their_turn(self):
+        with app.test_client() as c:
+            response = c.post('/blackjack/api/v1/games')
+            game_id = UUID(response.data.decode())
+            response = c.post(f'/blackjack/api/v1/games/{game_id}/players',
+                              json={'name': 'Ben Rooke'})
+            first_player_id = UUID(response.data.decode())
+            response = c.post(f'/blackjack/api/v1/games/{game_id}/players',
+                              json={'name': 'Dorian Clay'})
+            second_player_id = UUID(response.data.decode())
+            response = c.get(
+                f'/blackjack/api/v1/games/{game_id}/players/{second_player_id}/hit'
+            )
+            self.assertTrue(response.status.startswith('502'))
+
+    def test_player_stay(self):
+        with app.test_client() as c:
+            response = c.post('/blackjack/api/v1/games')
+            game_id = UUID(response.data.decode())
+            response = c.post(f'/blackjack/api/v1/games/{game_id}/players',
+                              json={'name': 'Ben Rooke'})
+            c.post(f'/blackjack/api/v1/games/{game_id}/players',
+                   json={'name': 'Dorian Clay'})
+            player_id = UUID(response.data.decode())
+            game = room.get_game(game_id)
+            player = game.players[0]
+            self.assertEqual(0, len(player.cards))
+            response = c.get(
+                f'/blackjack/api/v1/games/{game_id}/players/{player_id}/stay')
+            self.assertEqual(0, len(player.cards))
+            self.assertNotEqual(player, game.current_player())
+
+    def test_player_stay_but_not_their_turn(self):
+        with app.test_client() as c:
+            response = c.post('/blackjack/api/v1/games')
+            game_id = UUID(response.data.decode())
+            response = c.post(f'/blackjack/api/v1/games/{game_id}/players',
+                              json={'name': 'Ben Rooke'})
+            c.post(f'/blackjack/api/v1/games/{game_id}/players',
+                   json={'name': 'Dorian Clay'})
+            player_id = UUID(response.data.decode())
+            game = room.get_game(game_id)
+            player = game.players[0]
+            self.assertEqual(0, len(player.cards))
+            response = c.get(
+                f'/blackjack/api/v1/games/{game_id}/players/{player_id}/stay')
+            self.assertEqual(0, len(player.cards))
+            self.assertNotEqual(player, game.current_player())
+            response = c.get(
+                f'/blackjack/api/v1/games/{game_id}/players/{player_id}/stay')
+            self.assertTrue(response.status.startswith('502'))
+
+    def test_get_game_state(self):
+        with app.test_client() as c:
+            response = c.post('/blackjack/api/v1/games')
+            game_id = UUID(response.data.decode())
+            response = c.post(f'/blackjack/api/v1/games/{game_id}/players',
+                              json={'name': 'Ben Rooke'})
+            first_player_id = UUID(response.data.decode())
+            response = c.post(f'/blackjack/api/v1/games/{game_id}/players',
+                              json={'name': 'Dorian Clay'})
+            second_player_id = UUID(response.data.decode())
+            response = c.get(
+                f'/blackjack/api/v1/games/{game_id}/players/{first_player_id}')
+            self.assertEqual(
+                'Ben Rooke',
+                response.json[GameMetadata.Keys.you][PlayerMetadata.Keys.name])
+            self.assertEqual(
+                'Dorian Clay', response.json[GameMetadata.Keys.players][0][
+                    PlayerMetadata.Keys.name])
+            response = c.get(
+                f'/blackjack/api/v1/games/{game_id}/players/{second_player_id}')
+            self.assertEqual(
+                'Dorian Clay',
+                response.json[GameMetadata.Keys.you][PlayerMetadata.Keys.name])
+            self.assertEqual(
+                'Ben Rooke', response.json[GameMetadata.Keys.players][0][
+                    PlayerMetadata.Keys.name])
 
 
 if __name__ == '__main__':
