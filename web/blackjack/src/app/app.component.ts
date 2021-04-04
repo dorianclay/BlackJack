eb05709dab18a779b483ff8d22286e58d64384ea
Dorian Clay <dorian@dorianclay.com>
2021-04-04 11:30:52 -0700

    Replace main app component logic to just be a placeholder for router outlet.

--- web/blackjack/src/app/app.component.ts
+++ web/blackjack/src/app/app.component.ts
@@ -1,23 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { PlayerModel } from './player/player-model';
-import { PlayerMetadata } from './player/player-metadata';
-import { Card, Suit } from './card';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'blackjack';
-  playerModel!: PlayerModel;
-
-  ngOnInit() {
-    const playerMetadata = new PlayerMetadata('id', 'Dorian Clay');
-    const cards = [
-      new Card(Suit.SPADES, 1),
-      new Card(Suit.SPADES, 11),
-    ]
-    this.playerModel = new PlayerModel(playerMetadata, cards, 21);
-  }
+export class AppComponent {
+  constructor() {}
 }
