9a8ed2f7fcba3805cc8b69bbfcbbc8c0932b5c1b
Dorian Clay <thedorianmonster@gmail.com>
2021-04-10 11:21:35 -0700

    Remove unnecessary logic in card model.

--- web/blackjack/src/app/card.ts
+++ web/blackjack/src/app/card.ts
@@ -1,10 +1,3 @@
-export enum Suit {
-  CLUBS = 1,
-  DIAMONDS = 2,
-  HEARTS = 3,
-  SPADES = 4
-}
-
 export class Card {
-  constructor(public suit: Suit, public value: number) {}
+  constructor(public suit: string, public value: string) {}
 }
