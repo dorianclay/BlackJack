2067e14b1f1d537be128a734bbb651de406c2537
Dorian Clay <dorian@dorianclay.com>
2021-07-28 19:19:44 +0000

    Add CLI verbosity.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+echo *** Copying the blackjack service...
 sudo cp blackjack.service /etc/systemd/system
+
+echo *** Reloading daemons...
 sudo systemctl daemon-reload
 sudo systemctl start blackjack.service
+
+echo *** Copying ngnix configuration...
 sudo apt install nginx
 sudo cp blackjack.conf /etc/nginx/conf.d
+
+echo *** Done, all set.
 
