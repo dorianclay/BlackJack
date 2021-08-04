b7dc8c3506340fb6d566d495f25555066abdb8d5
Dorian Clay <dorian@dorianclay.com>
2021-08-04 13:49:34 -0700

    Clean up styling.

--- lint.sh
+++ lint.sh
@@ -2,6 +2,9 @@
 
 os="$(uname -s)"
 case "${os}" in
-    Darwin*)    find . -type f -name "*.py" | xargs python3 -m yapf --style="google" "${1:--i}";;
-    *)     find . -type f -name "*.py" | xargs yapf --style="google" "${1:--i}";;
+    Darwin*)    
+        find . -type f -name "*.py" | xargs python3 -m yapf --style="google" "${1:--i}";;
+    
+    *)     
+        find . -type f -name "*.py" | xargs yapf --style="google" "${1:--i}";;
 esac
